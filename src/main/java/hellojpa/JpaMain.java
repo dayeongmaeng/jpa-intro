99bcd5a4ce38a5c29244f62224e1cdee8d394314
dayeongmaeng <dayeongmaeng@gmail.com>
2022-10-05 17:05:06 +0900

    영속성 컨텍스트1

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,13 +18,16 @@ public class JpaMain {
 
         try {
 
-//            Member findMember = em.find(Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .getResultList();
+            //비영송
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
             
-            for (Member member : result){
-                System.out.println("member.getName() = " + member.getName());
-            }
+            //영속
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+            System.out.println("=== AFTER ===");
+
             tx.commit();
         }catch (Exception e){
             tx.rollback();
