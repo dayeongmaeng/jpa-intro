712e0f8cae755e4028b89b8b094afe1227b75b75
dayeongmaeng <dayeongmaeng@gmail.com>
2023-02-08 15:18:34 +0900

    feat : JPA 쿼리 방법 소개

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,13 +1,9 @@
 package hellojpa;
 
-import org.hibernate.Hibernate;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.time.LocalDateTime;
-import java.util.List;
 
 public class JpaMain {
 
@@ -22,38 +18,19 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddreess(new Address("homeCity", "street", "10000"));
+            //Criteria 사용 준비
+//            CriteriaBuilder cb = em.getCriteriaBuilder();
+//            CriteriaQuery<Member> query = cb.createQuery(Member.class);
+//            //루트 클래스 (조회를 시작할 클래스)
+//            Root<Member> m = query.from(Member.class);
+//            //쿼리 생성
+//            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
+//            List<Member> resultList = em.createQuery(cq).getResultList();
 
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("============= START =============");
-            Member findMember = em.find(Member.class, member.getId());
-
-//            Addreess a = findMember.getHomeAddreess();
-//            findMember.setHomeAddreess(new Addreess("newCity", a.getStreet(), a.getZipcode()));
-//
-//            ///치킨 -> 한식 단순 스트링
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("샐러드");
-
-//            //객체 수정
-//            findMember.getAddressHistory().remove(new AddreessEntity("old1", "street", "10000"));
-//            findMember.getAddressHistory().add(new AddreessEntity("newCity1", "street", "10000"));
+            em.createNativeQuery("select MEMBER_ID, city, street, zipcode, USERNAME from MEMBER")
+                    .getResultList();
 
             tx.commit();
-
         } catch (Exception e) {
             tx.rollback();
             e.printStackTrace();
